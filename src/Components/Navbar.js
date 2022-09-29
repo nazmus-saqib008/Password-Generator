607e9099aa3b39bb949cf812635e90f997e7bd9d
nazmus-saqib008 <iamnazmussaqib@outlook.com>
2022-09-30 01:27:48 +0530

    Published

---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div>
+            <nav className="navbar bg-info bg-gradient">
+                <div className="container-fluid">
+                    <span className="navbar-brand mb-0 h1 fw-bold fs-4">Welcome to Password Generator</span>
+                </div>
+            </nav>
+        </div>
+    )
+}
